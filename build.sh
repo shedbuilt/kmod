d304c71abdaf8fd2caf3d8e30d5446538e3fc6ba
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:24:51 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@
             --with-rootlibdir=/lib \
             --with-xz              \
             --with-zlib
-make -j $SHED_NUMJOBS
-make DESTDIR=${SHED_FAKEROOT} install
-mkdir -v ${SHED_FAKEROOT}/sbin
+make -j $SHED_NUM_JOBS
+make DESTDIR=${SHED_FAKE_ROOT} install
+mkdir -v ${SHED_FAKE_ROOT}/sbin
 for target in depmod insmod lsmod modinfo modprobe rmmod; do
-    ln -sv ../bin/kmod ${SHED_FAKEROOT}/sbin/$target
+    ln -sv ../bin/kmod ${SHED_FAKE_ROOT}/sbin/$target
 done
-ln -sv kmod ${SHED_FAKEROOT}/bin/lsmod
+ln -sv kmod ${SHED_FAKE_ROOT}/bin/lsmod
